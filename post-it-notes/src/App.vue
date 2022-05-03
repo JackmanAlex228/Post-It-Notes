ce462ce82f708b590a931c8ea3a7a8bf216a3eef
JackmanAlex228 <jackman.alex.w@gmail.com>
2022-05-02 18:44:57 -0600

    added firebase config and test

--- post-it-notes/src/App.vue
+++ post-it-notes/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddUser/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AddUser from './components/AddUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    AddUser
   }
 }
 </script>
---
+++ post-it-notes/src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for other Firebase products
+
+// Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBC_JwBmErbisRIje2MmxoW0jtOdbO4PdI",
+	authDomain: "post-it-notes-d7030.firebaseapp.com",
+	projectId: "post-it-notes-d7030",
+	storageBucket: "post-it-notes-d7030.appspot.com",
+	messagingSenderId: "639484237355",
+	appId: "1:639484237355:web:97bc217fcc56c438c8bc6e",
+	measurementId: "G-GSWFM7Q35M"
+};
+
+// Initialize Firebase;
+const app = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
+export const analytics = getAnalytics(app);
